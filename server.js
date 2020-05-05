2209d293b292c659b0bdc203fbe07f7b9195ded9
Ahmed Chiaeb <ahmed.chaieb@epitech.eu>
2020-05-05 23:41:03 +0200

    ff

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var MongoClient = require('mongodb').MongoClient;
 
 MongoClient.connect('mongodb://127.0.0.1:27042/',{useUnifiedTopology: true}, function(err, db) {
 	if(err){
-		console.log('COnnection failed');
+		console.log('Connection failed');
 
 	}
 	else{
